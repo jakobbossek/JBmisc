8fa451567ffbdf78edb14b0686963b5d26a3ebea
Jakob Bossek <info@jakobbossek.de>
2014-03-19 23:29:53 +0100

    move which.max2 to its own R source file

--- R/which.min2.R
+++ R/which.min2.R
@@ -16,24 +16,6 @@ which.min2 = function(x, type = "first") {
   return(get(idx, type))
 }
 
-#' More conventient function to get the index of the minimal index in a numeric vector.
-#' (base::which.min only returns the first position of the minimum value).
-#'
-#' @param x [\code{vector}]\cr
-#'   Source vector with numeric values.
-#' @param type [\code{character}]\cr
-#'   String indicating how to handle multiple occurences of the maximal value. Possible values are
-#'   "first", which leads to the know behavior of base::which.max, "last" which returns the index
-#'   of the last occurence of the maximal element, "random", which chooses the position by dice roling
-#'   and finally all, which returns a vector of all positions.
-#' @return [\code{vector}]
-#'   Position(s) of the occurence(s) of the maximal value.
-#' @export
-which.max2 = function(x, type = "first") {
-  idx = which(x == max(x))
-  return(get(idx, type))
-}
-
 # helper for which.{min,max}2
 get = function(x, type) {
   switch(type,
