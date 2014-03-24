420879b15449980a1017fd5de9ec50b4ef823dc7
Jakob Bossek <info@jakobbossek.de>
2014-03-24 12:19:09 +0100

    add test for usedFactors

---
+++ inst/tests/test_usedFactors.R
@@ -0,0 +1,11 @@
+context("usedFactors")
+
+test_that("usedFactors returns in fact the currently used factor levels", {
+  used.levels = letters[1:3]
+  all.levels = letters
+  x = factor(x = used.levels, levels = all.levels)
+  expect_equal(sort(usedFactors(x)), used.levels)
+
+  x = factor(x = used.levels)
+  expect_equal(used.levels, levels(x))
+})
